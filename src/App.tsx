ad21a850af72a2a229bfb16d6521a466a3e06475
Weldin Dunn <weldindunn20@gmail.com>
2022-04-05 15:08:42 -0400

    fixed staging error

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <div>Jingqing Liu add name</div>
+<<<<<<< HEAD
             <div>I&apos;m Weldin Dunn, and I approve this message.</div>
+=======
             <div>Zhiwen Zhu add name</div>
+>>>>>>> 7064db3819983e074682949df5e2298133d9ac2c
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
