1be83e1aa67164b25ae10c98a0714bed6d74da27
Weldin Dunn <weldindunn20@gmail.com>
2022-04-07 17:39:21 -0400

    Deleting interfaces to fix error

--- src/interfaces/Plan.ts
+++
@@ -1,9 +0,0 @@
-import { Course } from "./Course";
-import { Semester } from "./Semester";
-
-export interface Plan {
-    name: string;
-    semester: Semester[];
-    requirements: Course[];
-    floating_courses: Course[];
-}
