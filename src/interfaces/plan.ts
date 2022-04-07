1d51ec0da155e97dcf4732e690671d6d5a73b0c1
Weldin Dunn <weldindunn20@gmail.com>
2022-04-07 15:32:57 -0400

    Worked with Zhiwen to setup layout for plans

--- src/interfaces/plan.ts
+++ src/interfaces/plan.ts
@@ -1,11 +1,10 @@
-import { Semester } from "./semester";
-import { Course } from "./course";
+import { Course } from "./Course";
+import { Semester } from "./Semester";
 
-/** A representation of a Plan within the College Planner **/
 export interface Plan {
-    name: string; //Name of Plan
-    semesters: Semester[]; //Semesters within the plan
-    requirements: Course[]; //Required courses for student
-    taken_courses: Course[]; //Courses the student has already taken
-    floating_courses: Course[]; //Courses not assigned to any semester
+    name: string;
+    semesters: Semester[];
+    requirements: Course[];
+    taken_courses: Course[];
+    floating_courses: Course[];
 }
