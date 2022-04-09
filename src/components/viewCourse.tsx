50f01dc77cd41f70aadf399c94d6145a49530c5d
Zhiwen Zhu <zhiwenzhu@Zhiwens-MacBook-Pro.local>
2022-04-08 23:56:35 -0400

    sketch basic layout of course table

--- src/components/viewCourse.tsx
+++ src/components/viewCourse.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row, Table } from "react-bootstrap";
 import { Course } from "../interfaces/course";
 
 export function ViewCourse({ course }: { course: Course }): JSX.Element {
     return (
         <div>
             <Container>
-                <span>{course.name}</span>
+                <Table striped borderless hover>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <Row>
+                                    <Col>{course.id}</Col>
+                                    <Col>{course.name}</Col>
+                                    <Col>{course.description}</Col>
+                                    <Col>{course.credits}</Col>
+                                </Row>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
             </Container>
         </div>
     );
