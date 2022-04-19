1b79591b5d0e78e98b2aba3ca40af3d6be336cb9
Jingqing Liu <jingqing@Jingqings-MacBook-Pro.local>
2022-04-18 22:05:55 -0400

    add course section

--- src/components/listCourses.tsx
+++ src/components/listCourses.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Table } from "react-bootstrap";
+import { Button, Container, Table, Form } from "react-bootstrap";
 import { Course } from "../interfaces/course";
 import { EditCourseModal } from "./EditCourseModal";
 //import { ViewCourse } from "./viewCourse";
@@ -36,6 +36,33 @@ export function ListCourses({ semesterCourses }: Courses): JSX.Element {
         setShowAddModal(false);
     }
 
+    const [id, setId] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [credits, setCredits] = useState<string>("");
+
+    function addMovie(newCourse: Course) {
+        const existing = courses.find(
+            (course: Course): boolean => course.id === newCourse.id
+        );
+        if (existing === undefined) {
+            setCourses([...courses, newCourse]);
+        }
+    }
+
+    function saveAddChange() {
+        addMovie({
+            id: parseInt(id),
+            name: name,
+            credits: parseInt(credits),
+            description: description,
+            prerequisites: [],
+            isTaken: false,
+            isEditing: false,
+            breadthType: ""
+        });
+    }
+
     return (
         <div>
             <Table striped bordered hover>
@@ -65,6 +92,49 @@ export function ListCourses({ semesterCourses }: Courses): JSX.Element {
                             </td>
                         </tr>
                     ))}
+                    <tr key="CourseInput">
+                        <td>
+                            <Form.Control
+                                value={id}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setId(event.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <Form.Control
+                                value={name}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setName(event.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <Form.Control
+                                value={description}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setDescription(event.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <Form.Control
+                                value={credits}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setCredits(event.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <Button
+                                variant="primary"
+                                onClick={saveAddChange}
+                                className="button-style-2"
+                            >
+                                Add Course
+                            </Button>
+                        </td>
+                    </tr>
                 </tbody>
             </Table>
             <Container>
