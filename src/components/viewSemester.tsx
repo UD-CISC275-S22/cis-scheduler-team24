8a2e09c78ee81fbea5807db14182ce3826b5a153
Jingqing Liu <jingqing@wifi-roaming-128-4-72-114.wifi.udel.edu>
2022-04-28 10:45:40 -0400

    update edit plan name and delete semester

--- src/components/viewSemester.tsx
+++ src/components/viewSemester.tsx
@@ -1,65 +1,22 @@
 import React from "react";
 import { useState } from "react";
-import { Button, Container, Table } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 import { Semester } from "../interfaces/semester";
 import { Course } from "../interfaces/course";
 import { ListCourses } from "./listCourses";
-import { EditSemester } from "./editSemester";
 
 export function ViewSemester({
-    semester,
-    removeSemester,
-    setSemesterName
+    semester
 }: {
     semester: Semester;
-    removeSemester: (id: number) => void;
-    setSemesterName: (id: number, name: string) => void;
 }): JSX.Element {
     const [courses /*, setcourses*/] = useState<Course[]>(semester.courses);
-    const [isEditing, setEditing] = useState<boolean>(false);
-
-    function deleteSemester(): void {
-        removeSemester(semester.id);
-    }
-
-    function openEdit(): void {
-        setEditing(!isEditing);
-    }
 
     return (
         <div>
             <Container>
                 <Table striped borderless hover>
                     <thead>
-                        <tr>
-                            <th>
-                                <span>
-                                    {isEditing ? (
-                                        <EditSemester
-                                            semester={semester}
-                                            setSemesterName={setSemesterName}
-                                            openEdit={openEdit}
-                                        ></EditSemester>
-                                    ) : (
-                                        <div>
-                                            {semester.name}
-                                            <Button
-                                                onClick={openEdit}
-                                                variant="empty"
-                                                className="me-8"
-                                            >
-                                                🖊
-                                            </Button>
-                                        </div>
-                                    )}
-                                </span>
-                            </th>
-                            <th>
-                                <Button onClick={deleteSemester}>
-                                    Remove Semester
-                                </Button>
-                            </th>
-                        </tr>
                         <tr>
                             <th>
                                 <ListCourses
